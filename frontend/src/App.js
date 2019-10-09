a602a3430c218bed1b79b2cc322af275bbda611c
CarbonylGroup <carbonylgp@gmail.com>
2019-10-09 14:11:59 -0400

    App name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function Dashboard () {
+export default function App () {
   const classes = useStyles();
   return (
     <div className={classes.root}>
