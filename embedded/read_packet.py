8d2671acc1104b5dcb970314f895cfc297a703d6
harrynull <harryyunull@gmail.com>
2019-10-24 18:07:27 -0400

    Optimize read_packet.py for higher efficiency

--- embedded/read_packet.py
+++ embedded/read_packet.py
@@ -1,34 +1,20 @@
-import pyshark
-
 def is_local_administered(mac):
     return bool(int(mac[1],16)& 0b10)
 
 def read_packet(args):
-    if args.f:
-        cap = pyshark.FileCapture(args.f)
-    elif args.i:
-        print("Collecting")
-        cap = pyshark.LiveCapture(interface=args.i)
-        cap.sniff(timeout=15)
-    else:
-        print("Specify either -i or -f.")
+    if not args.f:
+        print("No file is provided. Specify one with -f.")
         exit()
-        
+    
     mac_set = set()
     pkg_count = 0
-    for packet in cap:
-        pkg_count += 1
-        wlan = packet["WLAN"]
-        if wlan.fc_type_subtype == "29":
-            #print("[*] Ignore ACK") # ACK
-            continue
-        if not hasattr(wlan, 'ta'):
-            #print("[*] Package does not have ta", packet["WLAN"])
-            continue
-        mac = wlan.ta
-        seq = wlan.seq if hasattr(wlan, 'seq') else -1
-        #print(seq, mac)
-        mac_set.add(mac)
+    with open(args.f, encoding="utf8") as file:
+        for packet_line in file:
+            pkg_count += 1
+            mac_addresses = packet_line.split(' → ')
+            for mac in mac_addresses:
+                if len(mac)<17: continue
+                mac_set.add(mac[:17])
 
     universal = len([mac for mac in mac_set if not is_local_administered(mac)])
     return (pkg_count, len(mac_set), universal)
--- embedded/run.sh
+++ embedded/run.sh
@@ -1,10 +1,12 @@
 #/bin/bash
+filename="cap_$EPOCHSECONDS"
 {
     . ./.env
-    echo "[$(date)] Start monitoring with ${CARD}"
-    tshark -l -Ini ${CARD} -w cap.pcap -a duration:${SCAN_TIME} || (echo "[$(date)] Failed. Exiting."; exit 1)
-    echo "[$(date)] Reporting"
-    python3 report.py -f cap.pcap
-    rm cap.pcap
-    echo "[$(date)] Finished."
+    echo "[$(date)] Start monitoring with ${CARD} $filename"
+    #tshark -l -Ini ${CARD} -w cap.pcap -a duration:${SCAN_TIME} || (echo "[$(date)] Failed. Exiting."; exit 1)
+    tshark -l -Ini ${CARD} -o "gui.column.format:\"Source\",\"%us\",\"Destination\",\"%rd\"" -a duration:${SCAN_TIME} > $filename
+    echo "[$(date)] Reporting $filename"
+    python3 report.py -f $filename
+    rm $filename
+    echo "[$(date)] $filename Finished."
 } >> log 2>&1
