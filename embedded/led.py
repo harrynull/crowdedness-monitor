0c7278fe2d066ccdf2be12229aea384aa22d4c7b
harrynull <harryyunull@gmail.com>
2019-11-15 17:00:19 -0500

    Show IP at startup

--- embedded/led.py
+++ embedded/led.py
@@ -11,13 +11,18 @@ def print_str(s, device):
     with canvas(device) as draw:
         device.contrast(8)
         text(draw, (0,0), s, fill="white", font=proportional(LCD_FONT))
-def scroll_str(s, device):
+def scroll_str(s, device, scroll_delay=0.03):
     with canvas(device) as draw:
         device.contrast(8)
-        show_message(device, s, fill="white", font=proportional(LCD_FONT))
+        show_message(device, s, fill="white", font=proportional(LCD_FONT), scroll_delay=scroll_delay)
 
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90)
+
+from subprocess import check_output
+ip="IP Address: " + check_output(['hostname', '-I'])
+scroll_str(ip, device, 0.1)
+
 ad = "Real-time busyness of buildings:"
 ad2 = "Also available at cm.harrynull.tech"
 while True:
