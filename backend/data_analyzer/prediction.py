b508be7cd07a6d8688354248c1e20ec6d5e61b76
harrynull <harryyunull@gmail.com>
2019-11-25 16:58:14 -0500

    cache

--- backend/data_analyzer/prediction.py
+++ backend/data_analyzer/prediction.py
@@ -7,7 +7,14 @@ from app.database import Device
 from data_analyzer.crowdedness import generate_crowdedness_from_universal_mac_count
 
 
+cache = {}
+last_cache_time = {}
+
+
 def predict(device_id: int, time_now: int):
+    if device_id in cache and time.time() - last_cache_time[device_id] < 5*60:
+        return cache[device_id]
+        
     current_time = datetime.datetime.fromtimestamp(time_now)
     feature_weekday = [int(i == current_time.weekday()) for i in range(0, 7)]
     feature_hour = [int(i == current_time.hour) for i in range(0, 24)]
@@ -19,5 +26,10 @@ def predict(device_id: int, time_now: int):
         mac_counts = [int(joblib.load('models/gbm_' + str(device_id) + '_' + str(i) + '_model.pkl').predict(X)[0])
                      for i in range(1, 13)]
     except:
+        cache[device_id] = []
+        last_cache_time[device_id] = time.time()
         return []
-    return [generate_crowdedness_from_universal_mac_count(device, mac) for mac in mac_counts]
+    res = [generate_crowdedness_from_universal_mac_count(device, mac) for mac in mac_counts]
+    cache[device_id] = res
+    last_cache_time[device_id] = time.time()
+    return res
