899d54eb0b70b82089f69b369d26494bacb45c39
harrynull <harryyunull@gmail.com>
2019-11-26 13:55:51 -0500

    Avoid negative crowdedness

--- backend/data_analyzer/crowdedness.py
+++ backend/data_analyzer/crowdedness.py
@@ -11,7 +11,7 @@ def generate_crowdedness_from_universal_mac_count(device: Device, mac_count: int
     if min_mac is None:
         min_mac = umac
 
-    return int(min(int((umac - min_mac) / (max_mac - min_mac + 1) * 100), 100))
+    return int(max(min(int((umac - min_mac) / (max_mac - min_mac + 1) * 100), 100),0))
 
 
 def generate_crowdedness(device: Device, data: Data):
